e9941765c9de072b9cac25c1ca5ed9b6ab0c78fd
Arseniy Schkidina <gorobchyk@bk.ru>
2024-05-04 18:47:13 +0700

    feat: created script for nignt-mode

---
+++ js/night-mode.js
@@ -0,0 +1,19 @@
+// 1.Найти кнопку в const.
+// 2.Произвести прослушку клика.
+// 3.Обратиться к body и удалить ему класс "night"!
+const dayBtn = document.querySelector("#sun");
+const nightBtn = document.querySelector("#nightBtn");
+const body = document.querySelector("body")
+dayBtn.addEventListener("click", function () {
+    body.classList.remove("night");
+    nightBtn.classList.remove("active");
+    this.classList.add("active");
+});
+nightBtn.addEventListener("click", function () {
+    body.classList.add("night");
+    nightBtn.classList.add("active");
+    dayBtn.classList.remove("active");
+});
+
+
+
