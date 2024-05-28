f3fd238c3535e2be0d934b2a273ab0143d064056
Arseniy Schkidina <gorobchyk@bk.ru>
2024-05-28 17:41:47 +0700

    feat: add function for receiving data

--- js/notes.js
+++ js/notes.js
@@ -2,7 +2,9 @@
 // переменные 
 const form = document.querySelector("#form");
 const addNote = document.querySelector("#add-note");
-const allNotes = [];
+const allNotes = dataFromLocalStorage() || [];
+const keyFromLocalStorage = "notes";
+console.log(allNotes);
 
 // функции
 function preparationMeaning(inputElement) {
@@ -26,16 +28,35 @@ function pushNewNote(noteObj) {
     allNotes.push(noteObj);
 }
 
+function toJSONFormat(arrayNotes) {
+    return JSON.stringify(arrayNotes);
+}
+
+function saveToLocalStorage(key, item) {
+    localStorage.setItem(key, item);
+}
+
+function dataFromLocalStorage(key) {
+    const data = JSON.parse(localStorage.getItem(key));
+    return data;
+}
+
+
 // события 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const processedText = preparationMeaning(addNote);
     const note = newNoteObject(processedText);
     pushNewNote(note);
-    console.log(allNotes);
+    const dataNotes = toJSONFormat(allNotes); 
+    saveToLocalStorage(keyFromLocalStorage, dataNotes);
+
 })
 
-
+// 1. Получить данные из local storage.
+// 2. Переформатировать в JSON.
+// 3. Рендерить заметки на странице.
+// 4. 
 
 
 
